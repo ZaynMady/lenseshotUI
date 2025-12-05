a4492b3fcfc4024a4e854ca42332343d0b993724
ZaynMady <zeinmady2848@gmail.com>
2025-12-05 22:39:20 +0200

    feat: integrate Sentry for error tracking and monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://7545045a7898ac18b724e65ced0a1165@o4510484098580480.ingest.de.sentry.io/4510484107952208",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<Sentry.ErrorBoundary 
+    fallback={<div className="error-screen">Oops! Something went wrong.</div>}
+    showDialog // This adds a popup asking the user what happened
+  >
+    <App />
+  </Sentry.ErrorBoundary>
+  );
